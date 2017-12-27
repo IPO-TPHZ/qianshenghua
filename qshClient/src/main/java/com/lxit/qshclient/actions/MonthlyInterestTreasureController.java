12977f8d934669ae227b18003e10ad9efd664e64
trz <2565147288@qq.com>
2017-12-27 15:07:39 +0800

    修改之后提交

---
+++ qshClient/src/main/java/com/lxit/qshclient/actions/MonthlyInterestTreasureController.java
@@ -0,0 +1,28 @@
+package com.lxit.qshclient.actions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MonthlyInterestTreasureController {
+	@RequestMapping("/monthly")
+	public String monthlyr() {
+		return "yuelibao.html";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/monthlyInterest")
+	public String monthlyInterest() {
+		System.out.println("进入查询月利宝");
+		return "true";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/monthlyInterestInster")
+	public String monthlyInterestInster() {
+		System.out.println("进入投资月利宝");
+		return "true";
+	}
+}
