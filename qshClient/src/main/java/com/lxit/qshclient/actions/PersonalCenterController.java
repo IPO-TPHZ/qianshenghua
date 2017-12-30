a8514c1a62bb202dfab2abaab7eccf245bb19bc6
twl <269927424@twl.com>
2017-12-30 15:23:08 +0800

    个人中心页面

---
+++ qshClient/src/main/java/com/lxit/qshclient/actions/PersonalCenterController.java
@@ -0,0 +1,17 @@
+package com.lxit.qshclient.actions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PersonalCenterController {
+	/*@RequestMapping("/personCenter")
+	public String personCenter(){
+		return "myGeRenZhongXin.html";
+	}
+	
+	@RequestMapping(value="/myWoDeZhangHu.html")
+	public String myWoDeZhangHu(){
+		return "myWoDeZhangHu.html";
+	}*/
+}
